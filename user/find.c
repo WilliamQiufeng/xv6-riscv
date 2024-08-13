a31f6965d92783081eb906e9fd4b0324d887758f
WilliamQiufeng <williamqiufeng@outlook.com>
2024-08-13 22:27:11 +0800

    Add find

---
+++ user/find.c
@@ -0,0 +1,81 @@
+//
+// Created by willi on 24-8-13.
+//
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+char *
+fmtname(char *path) {
+    static char buf[DIRSIZ + 1];
+    char *p;
+
+    // Find first character after last slash.
+    for (p = path + strlen(path); p >= path && *p != '/'; p--) {
+    }
+    p++;
+    strcpy(buf, p);
+    return buf;
+}
+
+void find(char *path, char *name) {
+    // printf("Finding %s under %s\n", name, path);
+    char buf[512], *p;
+    int fd;
+    struct dirent de;
+    struct stat st;
+
+    if ((fd = open(path, 0)) < 0) {
+        fprintf(2, "find: cannot open %s\n", path);
+        return;
+    }
+
+    if (fstat(fd, &st) < 0) {
+        fprintf(2, "find: cannot stat %s\n", path);
+        close(fd);
+        return;
+    }
+
+    switch (st.type) {
+        case T_FILE:
+            char* file_name = fmtname(path);
+            // printf("Cmp %s, %s\n", file_name, name);
+            if (strcmp(file_name, name) == 0) {
+                printf("%s\n", path);
+            }
+            break;
+
+        case T_DIR:
+            if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf) {
+                printf("find: path too long\n");
+                break;
+            }
+            strcpy(buf, path);
+            p = buf + strlen(buf);
+            *p++ = '/';
+            while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+                // printf("%d %s\n", de.inum, de.name);
+                if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+                    continue;
+                memmove(p, de.name, DIRSIZ);
+                p[DIRSIZ] = 0;
+                if (stat(buf, &st) < 0) {
+                    printf("find: cannot stat %s\n", buf);
+                    continue;
+                }
+                find(buf, name);
+            }
+            break;
+    }
+    close(fd);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3)
+        exit(1);
+    char *dir = argv[1];
+    char *name = argv[2];
+    find(dir, name);
+    exit(0);
+}
